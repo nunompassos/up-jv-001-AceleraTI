70bce9f926b2ca04768d5e4d58fb5cf4310b6d29
Nuno Passos <nuno.passos@teapayment.co.uk>
2023-05-15 19:49:10 +0100

    Aula sobre classes e métodos

--- src/main/java/tech/ada/velha/App.java
+++ src/main/java/tech/ada/velha/App.java
@@ -10,7 +10,6 @@ public class App {
         Tabuleiro t = new Tabuleiro(new String("X"), "O");
         final Scanner sc = new Scanner(System.in);
 
-        String pecaAntiga = "";
         boolean jogadaInvalida;
 
         t.imprimirTabuleiro();
@@ -18,95 +17,13 @@ public class App {
             do {
                 jogadaInvalida = false;
                 System.out.print("Insira a sua peça e a posição (X;4): ");
-                final String peca = sc.next();
-                final String[] split = peca.split(";");
-                if ((!"X".equals(split[0]) && !"O".equals(split[0])) || pecaAntiga.equals(split[0])) {
-                    System.out.println("Peça não é válida!");
-                    jogadaInvalida = true;
-                    continue;
-                }
-                if (split[1].compareTo("0") < 0 && split[1].compareTo("9") > 0) {
-                    System.out.println("Posição inválida!");
+                final String jogada = sc.next();
+                if (!t.validaJogada(jogada)) {
                     jogadaInvalida = true;
                     continue;
                 }
                 System.out.println("Jogada válida");
-                pecaAntiga = split[0];
-                int posicao = Integer.parseInt(split[1]);
-                switch (posicao) {
-                    case 1:
-                        if (!t.getPosicoes()[0][0].equals("_")) {
-                            System.out.println("Posição não válida");
-                            jogadaInvalida = true;
-                            continue;
-                        }
-                        t.getPosicoes()[0][0] = pecaAntiga;
-                        break;
-                    case 2:
-                        if (!t.getPosicoes()[0][1].equals("_")) {
-                            System.out.println("Posição não válida");
-                            jogadaInvalida = true;
-                            continue;
-                        }
-                        t.getPosicoes()[0][1] = pecaAntiga;
-                        break;
-                    case 3:
-                        if (!t.getPosicoes()[0][2].equals("_")) {
-                            System.out.println("Posição não válida");
-                            jogadaInvalida = true;
-                            continue;
-                        }
-                        t.getPosicoes()[0][2] = pecaAntiga;
-                        break;
-                    case 4:
-                        if (!t.getPosicoes()[1][0].equals("_")) {
-                            System.out.println("Posição não válida");
-                            jogadaInvalida = true;
-                            continue;
-                        }
-                        t.getPosicoes()[1][0] = pecaAntiga;
-                        break;
-                    case 5:
-                        if (!t.getPosicoes()[1][1].equals("_")) {
-                            System.out.println("Posição não válida");
-                            jogadaInvalida = true;
-                            continue;
-                        }
-                        t.getPosicoes()[1][1] = pecaAntiga;
-                        break;
-                    case 6:
-                        if (!t.getPosicoes()[1][2].equals("_")) {
-                            System.out.println("Posição não válida");
-                            jogadaInvalida = true;
-                            continue;
-                        }
-                        t.getPosicoes()[1][2] = pecaAntiga;
-                        break;
-                    case 7:
-                        if (!t.getPosicoes()[2][0].equals(" ")) {
-                            System.out.println("Posição não válida");
-                            jogadaInvalida = true;
-                            continue;
-                        }
-                        t.getPosicoes()[2][0] = pecaAntiga;
-                        break;
-                    case 8:
-                        if (!t.getPosicoes()[2][1].equals(" ")) {
-                            System.out.println("Posição não válida");
-                            jogadaInvalida = true;
-                            continue;
-                        }
-                        t.getPosicoes()[2][1] = pecaAntiga;
-                        break;
-                    case 9:
-                        if (!t.getPosicoes()[2][2].equals(" ")) {
-                            System.out.println("Posição não válida");
-                            jogadaInvalida = true;
-                            continue;
-                        }
-                        t.getPosicoes()[2][2] = pecaAntiga;
-                        break;
-                }
+                t.aplicarJogada();
 
             } while (jogadaInvalida);
 
--- src/main/java/tech/ada/velha/Tabuleiro.java
+++ src/main/java/tech/ada/velha/Tabuleiro.java
@@ -9,10 +9,15 @@ public class Tabuleiro {
     private String[][] posicoes = {{"_", "_", "_"}, {"_", "_", "_"}, {" ", " ", " "}};
     private String peca1;
     private String peca2;
+
+    private String pecaAntiga = "";
     private String vencedor = "Empate";
+    private String pecaAtual = "";
+    private int posicaoAtual = -1;
 
     public Tabuleiro() {
     }
+
     public Tabuleiro(String peca1, String peca2) {
         this.peca1 = peca1;
         this.peca2 = peca2;
@@ -58,7 +63,7 @@ public class Tabuleiro {
 
     private boolean validarLinhas() {
         boolean linha = false;
-        for (String[] l: posicoes) {
+        for (String[] l : posicoes) {
             if (l[0].equals(l[1]) && l[1].equals(l[2]) && !l[0].equals("_") && !l[0].equals(" ")) {
                 linha = true;
                 vencedor = l[0];
@@ -71,10 +76,7 @@ public class Tabuleiro {
     private boolean validarColunas() {
         boolean coluna = false;
         for (int i = 0; i < 3; i++) {
-            if (posicoes[0][i].equals(posicoes[1][i]) &&
-                posicoes[1][i].equals(posicoes[2][i]) &&
-                !posicoes[0][i].equals("_") &&
-                !posicoes[0][i].equals(" ")) {
+            if (posicoes[0][i].equals(posicoes[1][i]) && posicoes[1][i].equals(posicoes[2][i]) && !posicoes[0][i].equals("_") && !posicoes[0][i].equals(" ")) {
                 coluna = true;
                 vencedor = posicoes[0][i];
                 break;
@@ -84,17 +86,11 @@ public class Tabuleiro {
     }
 
     private boolean validarDiagonais() {
-        if (posicoes[0][0].equals(posicoes[1][1]) &&
-            posicoes[1][1].equals(posicoes[2][2]) &&
-            !posicoes[0][0].equals("_") &&
-            !posicoes[0][0].equals(" ")) {
+        if (posicoes[0][0].equals(posicoes[1][1]) && posicoes[1][1].equals(posicoes[2][2]) && !posicoes[0][0].equals("_") && !posicoes[0][0].equals(" ")) {
             vencedor = posicoes[1][1];
             return true;
         }
-        if (posicoes[0][2].equals(posicoes[1][1]) &&
-            posicoes[1][1].equals(posicoes[2][0]) &&
-            !posicoes[0][2].equals("_") &&
-            !posicoes[0][2].equals(" ")) {
+        if (posicoes[0][2].equals(posicoes[1][1]) && posicoes[1][1].equals(posicoes[2][0]) && !posicoes[0][2].equals("_") && !posicoes[0][2].equals(" ")) {
             vencedor = posicoes[1][1];
             return true;
         }
@@ -107,6 +103,124 @@ public class Tabuleiro {
     }
 
     public String getVencedor() {
-        return this.vencedor;
+        if (this.vencedor.equals("Empate")) {
+            return "Jogo terminou empatado!";
+        }
+        return "O vencedor utilizou a peça " + vencedor;
+    }
+
+    public boolean validaJogada(final String jogada) {
+
+        final String[] jogadas = jogada.split(";");
+        if ((!peca1.equals(jogadas[0]) && !peca2.equals(jogadas[0])) || pecaAntiga.equals(jogadas[0])) {
+            System.out.println("Peça não é válida!");
+            return false;
+        }
+        if (jogadas[1].compareTo("0") < 0 && jogadas[1].compareTo("9") > 0) {
+            System.out.println("Posição inválida!");
+            return false;
+        }
+        pecaAtual = jogadas[0];
+        posicaoAtual = Integer.parseInt(jogadas[1]);
+
+        return validaPosicao();
+    }
+
+    private boolean validaPosicao() {
+        boolean result = true;
+        switch (posicaoAtual) {
+            case 1 -> {
+                if (!posicoes[0][0].equals("_")) {
+                    result = false;
+                }
+            }
+            case 2 -> {
+                if (!posicoes[0][1].equals("_")) {
+                    result = false;
+                }
+            }
+            case 3 -> {
+                if (!posicoes[0][2].equals("_")) {
+                    result = false;
+                }
+            }
+            case 4 -> {
+                if (!posicoes[1][0].equals("_")) {
+                    result = false;
+                }
+            }
+            case 5 -> {
+                if (!posicoes[1][1].equals("_")) {
+                    result = false;
+                }
+            }
+            case 6 -> {
+                if (!posicoes[1][2].equals("_")) {
+                    result = false;
+                }
+            }
+            case 7 -> {
+                if (!posicoes[2][0].equals(" ")) {
+                    result = false;
+                }
+            }
+            case 8 -> {
+                if (!posicoes[2][1].equals(" ")) {
+                    result = false;
+                }
+            }
+            case 9 -> {
+                if (!posicoes[2][2].equals(" ")) {
+                    result = false;
+                }
+            }
+        }
+        if (!result) {
+            System.out.println("Posição não válida");
+            return false;
+        }
+        return true;
+    }
+
+    public void aplicarJogada() {
+        pecaAntiga = pecaAtual;
+        switch (posicaoAtual) {
+            case 1 -> {
+                posicoes[0][0] = pecaAtual;
+                break;
+            }
+            case 2 -> {
+                posicoes[0][1] = pecaAtual;
+                break;
+            }
+            case 3 -> {
+                posicoes[0][2] = pecaAtual;
+                break;
+            }
+            case 4 -> {
+                posicoes[1][0] = pecaAtual;
+                break;
+            }
+            case 5 -> {
+                posicoes[1][1] = pecaAtual;
+                break;
+            }
+            case 6 -> {
+                posicoes[1][2] = pecaAtual;
+                break;
+            }
+            case 7 -> {
+                posicoes[2][0] = pecaAtual;
+                break;
+            }
+            case 8 -> {
+                posicoes[2][1] = pecaAtual;
+                break;
+            }
+            case 9 -> {
+                posicoes[2][2] = pecaAtual;
+                break;
+            }
+        }
     }
 }
